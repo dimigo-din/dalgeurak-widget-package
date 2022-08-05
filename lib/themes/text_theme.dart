4ed68fb13119c34a34582d2d12e17594f5ea97ad
doch13_ <dohui0922@dimigo.hs.kr>
2022-08-06 00:59:25 +0900

    feat: Exist Common Widgets

---
+++ lib/themes/text_theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'color_theme.dart';
+
+
+final btnTitle1 = TextStyle(
+    fontSize: 14, color: dalgeurakBlueOne, fontFamily: 'Pretendard', fontWeight: FontWeight.w700);
+final btnTitle2 = TextStyle(
+    fontSize: 17, color: dalgeurakBlueOne, fontFamily: 'Pretendard', fontWeight: FontWeight.w700);
+
+final checkBox = TextStyle(
+    fontSize: 16, color: Colors.black, fontFamily: 'Pretendard', fontWeight: FontWeight.w500);
+
+final warningDialog_message = TextStyle(
+    fontSize: 16, color: Colors.black, fontFamily: 'Pretendard', fontWeight: FontWeight.w500);
+final warningDialog_subMessage = TextStyle(
+    fontSize: 14, color: dalgeurakGrayThree, fontFamily: 'Pretendard', fontWeight: FontWeight.w500);
+final noticeDialog_message = TextStyle(
+    fontSize: 13, color: Colors.black, fontFamily: 'Pretendard', fontWeight: FontWeight.w500);
+final noticeDialog_menu = TextStyle(
+    fontSize: 14, color: Colors.white, fontFamily: 'Pretendard', fontWeight: FontWeight.w500);
+final inquiryDialog_emailTitle = TextStyle(
+    fontSize: 16, color: dalgeurakGrayFour, fontFamily: 'Pretendard', fontWeight: FontWeight.w500);
+final inquiryDialog_emailAddress = TextStyle(
+    fontSize: 14, color: Colors.black, fontFamily: 'Pretendard', fontWeight: FontWeight.w500);
+final dialogTitle = TextStyle(
+    fontSize: 17, color: Colors.black, fontFamily: 'Pretendard', fontWeight: FontWeight.w700);
+final dialogBtn = TextStyle(
+    fontSize: 14, color: Colors.white, fontFamily: 'Pretendard', fontWeight: FontWeight.w500);
+final overlayAlert = TextStyle(
+    fontSize: 14, color: Colors.black, fontFamily: 'Pretendard', fontWeight: FontWeight.w700);
+
+final windowTitle_subTitle = TextStyle(
+    fontSize: 20, color: Colors.black, fontFamily: 'Pretendard', fontWeight: FontWeight.w400);
+final windowTitle_title = TextStyle(
+    fontSize: 26, color: Colors.black, fontFamily: 'Pretendard', fontWeight: FontWeight.w700);
