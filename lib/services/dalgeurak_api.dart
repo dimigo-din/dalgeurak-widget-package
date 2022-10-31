71703810979667156f827a2fbeb514db721f2554
doch13_ <dohui0922@dimigo.hs.kr>
2022-10-31 15:06:02 +0900

    fix: getStudentList Wrong Cache Data

--- lib/services/dalgeurak_api.dart
+++ lib/services/dalgeurak_api.dart
@@ -13,7 +13,7 @@ class DalgeurakStudentListAPI {
     String? stringData = SharedPreference().getStudentList();
     int weekFirstDay = (nowTime.day - (nowTime.weekday - 1));
 
-    if (isMust || stringData == null || (json.decode(stringData))["weekFirstDay"] != _dalgeurakService.getCorrectDate(weekFirstDay)['day']) {
+    getStudentListInDimigoin() async {
       Map data = await _dalgeurakService.getAllStudentListInDimigoin();
 
       if (data['success']) {
@@ -22,10 +22,18 @@ class DalgeurakStudentListAPI {
       } else {
         _dalgeurakToast.show(data['content']);
       }
+    }
+
+
+    if (isMust || stringData == null || (json.decode(stringData))["weekFirstDay"] != _dalgeurakService.getCorrectDate(weekFirstDay)['day']) {
+      await getStudentListInDimigoin();
     } else {
       List originalData = (json.decode(stringData))['studentList'];
       List formattingData = [];
       originalData.forEach((element) => formattingData.add(DimigoinUser.fromJson(element)));
+
+      //2.0.2 버전에서 학생 데이터가 3중 중첩되었던 적이 있어, 중첩된 데이터가 캐시로 등록되었을 경우에는 새 데이터로 인터넷에서 불러오는 코드입니다.
+      if (formattingData.length > 1000) { await getStudentListInDimigoin(); }
 
       return formattingData;
     }
